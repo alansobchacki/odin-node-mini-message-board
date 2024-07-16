d630b8b1b26bea50784dd513f0a1ab6c997fe5f8
Alan Sobchacki <a.sobchack@gmail.com>
2024-07-16 19:42:03 -0300

    add basic visuals to website.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,24 @@ const router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
+    text: "Hi there! 👋 This app does not connect to a database, so your messages will disappear after a while.",
+    user: "Dennis",
+    added: "Sat, 13 Jul 2024 18:15:53 GMT",
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
+    text: "This app was built to practice HTTP methods (mainly GET and POST), setting up an Express skeleton, and basic deployment.",
+    user: "Dennis",
+    added: "Sat, 13 Jul 2024 18:17:51 GMT",
+  },
+  {
+    text: "More complex apps coming soon™",
+    user: "Dennis",
+    added: "Sat, 13 Jul 2024 18:22:31 GMT",
+  },
+  {
+    text: "spyder",
+    user: "Charlie",
+    added: "Sun, 14 Jul 2024 12:35:12 GMT",
   },
 ];
 
